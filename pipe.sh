d6f2c81389dd749d856833216108d9183da434fc
dknodes <luizamirlovaa@gmail.com>
2025-03-06 15:32:46 +0300

    Update pipe.sh

--- pipe.sh
+++ pipe.sh
@@ -58,6 +58,13 @@ install_pipe_node() {
     wget -O pop "https://dl.pipecdn.app/v0.2.8/pop"
     chmod +x pop
 
+    sudo ./pop \\
+    --ram ${ram} \\
+    --max-disk ${max_disk} \\
+    --cache-dir ${curr_dir}/download_cache \\
+    --pubKey ${soladdress} \\
+    --signup-by-referral-route e70d24f2d1553964
+
     # Create systemd service file for Pipe Node
     sudo tee /etc/systemd/system/pipe.service > /dev/null << EOF
 [Unit]
